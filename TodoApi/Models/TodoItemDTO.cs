22c78e2c6ff7cb46661cb4f21fd5fbbbbc7fc312
Pietro Oliveira <pietromartinso@gmail.com>
2022-04-19 11:09:14 -0300

    Simple TodoList Web API

---
+++ TodoApi/Models/TodoItemDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Models
+{
+    //Data Transfer Object: prevent over-posting
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
