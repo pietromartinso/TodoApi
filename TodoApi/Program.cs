0564eb1a96b067fc3d1ba841930aa13f555a1c8a
Pietro Oliveira <pietromartinso@gmail.com>
2022-04-19 13:52:43 -0300

    Adding front-end

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -3,25 +3,19 @@ using TodoApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
-//});
-
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-/*    app.UseSwagger();
-    app.UseSwaggerUI();*/
+
 }
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseHttpsRedirection();
 
