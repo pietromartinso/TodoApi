172133b3ccdfb5631cc098c414ff80c6607d2a81
Pietro Oliveira <pietromartinso@gmail.com>
2022-04-20 15:23:37 -0300

    AWS RDS Database

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
 {
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnectionString"));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("AWSRDSSQLServerConnectionString"));
 });
 
 var app = builder.Build();
