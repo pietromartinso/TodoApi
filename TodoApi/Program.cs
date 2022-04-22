2e11e7ddb5c8ba52052ed853f77fed83aa8af6bd
Pietro Oliveira <pietromartinso@gmail.com>
2022-04-22 20:03:56 -0300

    Merges

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,12 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Data;
+using System;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseKestrel()
+    .UseContentRoot(Directory.GetCurrentDirectory())
+    .UseIISIntegration();
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<TodoContext>(opt =>
 {
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("AWSRDSSQLServerConnectionString"));
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnectionString"));
 });
 
 var app = builder.Build();
